05e3dae1257f559c04178a4e8db1f508571614b3
Torben Mæhle <Torben@Torbens-MacBook-Air.local>
2017-01-13 20:40:21 +0100

    Initial Commit

---
+++ tester/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tester
+//
+//  Created by Torben Mæhle on 13/01/2017.
+//  Copyright © 2017 Torben Mæhle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
