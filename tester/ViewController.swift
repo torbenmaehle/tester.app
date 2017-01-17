62b7801fdfe95a5783858bf54c649d9f0d78a581
Torben Mæhle <Torben@Torbens-MacBook-Air.local>
2017-01-17 21:54:45 +0100

    Add in comment

--- tester/ViewController.swift
+++ tester/ViewController.swift
@@ -4,11 +4,23 @@
 //
 //  Created by Torben Mæhle on 13/01/2017.
 //  Copyright © 2017 Torben Mæhle. All rights reserved.
+// tester.app
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+ 
+    @IBOutlet weak var ageTextfield: UITextField!
+   
+    @IBOutlet weak var ageLabel: UILabel!
+    @IBAction func submitPressed(_ sender: Any) {
+     
+        let ageInCatYears = Int(ageTextfield.text!)! * 7
+     
+        ageLabel.text = String(ageInCatYears)
+
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
